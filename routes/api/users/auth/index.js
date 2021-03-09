6d56943c25fbc1c0fc0d5e2c360102ae96acbd7f
Viktoriia Melanchenko <victoriamelanchenko@gmail.com>
2021-03-09 23:20:17 +0200

    added user validation

--- routes/api/users/auth/index.js
+++ routes/api/users/auth/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { reg, login, logout } = require('../../../../controllers/users');
 const guard = require('../../../../helpers/guard');
+const validate = require('../validation');
 
-router.post('/registration', reg);
-router.post('/login', login);
+router.post('/registration', validate.reg, reg);
+router.post('/login', validate.login, login);
 router.post('/logout', guard, logout);
 
 module.exports = router;
--- routes/api/users/validation.js
+++ routes/api/users/validation.js
@@ -1,34 +1,41 @@
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// const schemaRegUser = Joi.object({
-//   email: Joi.string().email().required(),
-//   password: Joi.string().required(),
-// });
+const schemaRegUser = Joi.object({
+  //   email: Joi.string().email().required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  password: Joi.string().required(),
+});
 
-// const schemaLoginUser = Joi.object({
-//   email: Joi.string().email().required(),
-//   password: Joi.string().required(),
-// });
+const schemaLoginUser = Joi.object({
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  password: Joi.string().required(),
+});
 
-// const validate = (schema, obj, next) => {
-//   const { error } = schema.validate(obj);
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
 
-//   if (error) {
-//     console.log(error.details[0].path);
-//     console.log(error.details[0].context);
-//     const [{ message }] = error.details;
-//     return next({
-//       status: 400,
-//       message: `Field: ${message.replace(/"/g, '')}`,
-//     });
-//   }
-//   next();
-// };
+  if (error) {
+    console.log(error.details[0].path);
+    console.log(error.details[0].context);
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Field: ${message.replace(/"/g, '')}`,
+    });
+  }
+  next();
+};
 
-// module.exports.reg = (req, _res, next) => {
-//   return validate(schemaRegUser, req.body, next);
-// };
+module.exports.reg = (req, _res, next) => {
+  return validate(schemaRegUser, req.body, next);
+};
 
-// module.exports.login = (req, _res, next) => {
-//   return validate(schemaLoginUser, req.body, next);
-// };
+module.exports.login = (req, _res, next) => {
+  return validate(schemaLoginUser, req.body, next);
+};
